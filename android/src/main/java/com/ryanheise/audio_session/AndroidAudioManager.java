0624039a12e9e334a6ae9571e69a648d2e8994ac
Ryan Heise <ryan@ryanheise.com>
2020-12-08 17:30:39 +1100

    Convert Java TABs to 4 spaces.

--- android/src/main/java/com/ryanheise/audio_session/AndroidAudioManager.java
+++ android/src/main/java/com/ryanheise/audio_session/AndroidAudioManager.java
@@ -19,128 +19,128 @@ import java.util.List;
 import java.util.Map;
 
 public class AndroidAudioManager implements MethodCallHandler {
-	private static List<AndroidAudioManager> instances = new ArrayList<>();
-	private static AudioFocusRequestCompat audioFocusRequest;
-	private static BroadcastReceiver noisyReceiver;
+    private static List<AndroidAudioManager> instances = new ArrayList<>();
+    private static AudioFocusRequestCompat audioFocusRequest;
+    private static BroadcastReceiver noisyReceiver;
 
-	private Context applicationContext;
-	private BinaryMessenger messenger;
-	private MethodChannel channel;
-	private AudioManager audioManager;
+    private Context applicationContext;
+    private BinaryMessenger messenger;
+    private MethodChannel channel;
+    private AudioManager audioManager;
 
-	public AndroidAudioManager(Context applicationContext, BinaryMessenger messenger) {
-		this.applicationContext = applicationContext;
-		this.messenger = messenger;
-		channel = new MethodChannel(messenger, "com.ryanheise.android_audio_manager");
-		channel.setMethodCallHandler(this);
-		audioManager = (AudioManager)applicationContext.getSystemService(Context.AUDIO_SERVICE);
-		instances.add(this);
-	}
+    public AndroidAudioManager(Context applicationContext, BinaryMessenger messenger) {
+        this.applicationContext = applicationContext;
+        this.messenger = messenger;
+        channel = new MethodChannel(messenger, "com.ryanheise.android_audio_manager");
+        channel.setMethodCallHandler(this);
+        audioManager = (AudioManager)applicationContext.getSystemService(Context.AUDIO_SERVICE);
+        instances.add(this);
+    }
 
-	@Override
-	public void onMethodCall(final MethodCall call, final Result result) {
-		List<?> args = (List<?>)call.arguments;
-		switch (call.method) {
-		case "requestAudioFocus": {
-			result.success(requestAudioFocus(args));
-			break;
-		}
-		case "abandonAudioFocus": {
-			result.success(abandonAudioFocus());
-			break;
-		}
-		default: {
-			result.notImplemented();
-			break;
-		}
-		}
-	}
+    @Override
+    public void onMethodCall(final MethodCall call, final Result result) {
+        List<?> args = (List<?>)call.arguments;
+        switch (call.method) {
+        case "requestAudioFocus": {
+            result.success(requestAudioFocus(args));
+            break;
+        }
+        case "abandonAudioFocus": {
+            result.success(abandonAudioFocus());
+            break;
+        }
+        default: {
+            result.notImplemented();
+            break;
+        }
+        }
+    }
 
-	private boolean requestAudioFocus(List<?> args) {
-		if (audioFocusRequest != null) {
-			return true;
-		}
-		Map<?, ?> request = (Map<?, ?>)args.get(0);
-		AudioFocusRequestCompat.Builder builder = new AudioFocusRequestCompat.Builder((Integer)request.get("gainType"));
-		builder.setOnAudioFocusChangeListener(focusChange -> {
-			if (focusChange == AudioManager.AUDIOFOCUS_LOSS) abandonAudioFocus();
-			invokeMethod("onAudioFocusChanged", focusChange);
-		});
-		if (request.get("audioAttributes") != null) {
-			builder.setAudioAttributes(decodeAudioAttributes((Map<?, ?>)request.get("audioAttributes")));
-		}
-		if (request.get("willPauseWhenDucked") != null) {
-			builder.setWillPauseWhenDucked((Boolean)request.get("willPauseWhenDucked"));
-		}
-		audioFocusRequest = builder.build();
-		int status = AudioManagerCompat.requestAudioFocus(audioManager, audioFocusRequest);
-		boolean success = status == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+    private boolean requestAudioFocus(List<?> args) {
+        if (audioFocusRequest != null) {
+            return true;
+        }
+        Map<?, ?> request = (Map<?, ?>)args.get(0);
+        AudioFocusRequestCompat.Builder builder = new AudioFocusRequestCompat.Builder((Integer)request.get("gainType"));
+        builder.setOnAudioFocusChangeListener(focusChange -> {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS) abandonAudioFocus();
+            invokeMethod("onAudioFocusChanged", focusChange);
+        });
+        if (request.get("audioAttributes") != null) {
+            builder.setAudioAttributes(decodeAudioAttributes((Map<?, ?>)request.get("audioAttributes")));
+        }
+        if (request.get("willPauseWhenDucked") != null) {
+            builder.setWillPauseWhenDucked((Boolean)request.get("willPauseWhenDucked"));
+        }
+        audioFocusRequest = builder.build();
+        int status = AudioManagerCompat.requestAudioFocus(audioManager, audioFocusRequest);
+        boolean success = status == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
         if (success) {
             registerNoisyReceiver();
         }
         return success;
-	}
+    }
 
-	private boolean abandonAudioFocus() {
+    private boolean abandonAudioFocus() {
         unregisterNoisyReceiver();
-		if (audioFocusRequest == null) {
-			return true;
-		} else {
-			int status = AudioManagerCompat.abandonAudioFocusRequest(audioManager, audioFocusRequest);
-			audioFocusRequest = null;
-			return status == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
-		}
-	}
+        if (audioFocusRequest == null) {
+            return true;
+        } else {
+            int status = AudioManagerCompat.abandonAudioFocusRequest(audioManager, audioFocusRequest);
+            audioFocusRequest = null;
+            return status == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+        }
+    }
 
-	private void registerNoisyReceiver() {
-		if (noisyReceiver != null) return;
-		noisyReceiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
-					invokeMethod("onBecomingNoisy");
-				}
-			}
-		};
-		applicationContext.registerReceiver(noisyReceiver, new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY));
-	}
+    private void registerNoisyReceiver() {
+        if (noisyReceiver != null) return;
+        noisyReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
+                    invokeMethod("onBecomingNoisy");
+                }
+            }
+        };
+        applicationContext.registerReceiver(noisyReceiver, new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY));
+    }
 
-	private void unregisterNoisyReceiver() {
-		if (noisyReceiver == null) return;
-		applicationContext.unregisterReceiver(noisyReceiver);
-		noisyReceiver = null;
-	}
+    private void unregisterNoisyReceiver() {
+        if (noisyReceiver == null) return;
+        applicationContext.unregisterReceiver(noisyReceiver);
+        noisyReceiver = null;
+    }
 
-	private AudioAttributesCompat decodeAudioAttributes(Map<?, ?> attributes) {
-		AudioAttributesCompat.Builder builder = new AudioAttributesCompat.Builder();
-		if (attributes.get("contentType") != null) {
-			builder.setContentType((Integer)attributes.get("contentType"));
-		}
-		if (attributes.get("flags") != null) {
-			builder.setFlags((Integer)attributes.get("flags"));
-		}
-		if (attributes.get("usage") != null) {
-			builder.setUsage((Integer)attributes.get("usage"));
-		}
-		return builder.build();
-	}
+    private AudioAttributesCompat decodeAudioAttributes(Map<?, ?> attributes) {
+        AudioAttributesCompat.Builder builder = new AudioAttributesCompat.Builder();
+        if (attributes.get("contentType") != null) {
+            builder.setContentType((Integer)attributes.get("contentType"));
+        }
+        if (attributes.get("flags") != null) {
+            builder.setFlags((Integer)attributes.get("flags"));
+        }
+        if (attributes.get("usage") != null) {
+            builder.setUsage((Integer)attributes.get("usage"));
+        }
+        return builder.build();
+    }
 
-	public void dispose() {
-		instances.remove(this);
-		if (instances.size() == 0) {
-			abandonAudioFocus();
-		}
-		channel.setMethodCallHandler(null);
+    public void dispose() {
+        instances.remove(this);
+        if (instances.size() == 0) {
+            abandonAudioFocus();
+        }
+        channel.setMethodCallHandler(null);
         channel = null;
         applicationContext = null;
         messenger = null;
         audioManager = null;
-	}
+    }
 
-	private void invokeMethod(String method, Object... args) {
-		for (AndroidAudioManager instance : instances) {
-			ArrayList<Object> list = new ArrayList<Object>(Arrays.asList(args));
-			instance.channel.invokeMethod(method, list);
-		}
-	}
+    private void invokeMethod(String method, Object... args) {
+        for (AndroidAudioManager instance : instances) {
+            ArrayList<Object> list = new ArrayList<Object>(Arrays.asList(args));
+            instance.channel.invokeMethod(method, list);
+        }
+    }
 }
--- android/src/main/java/com/ryanheise/audio_session/AudioSessionPlugin.java
+++ android/src/main/java/com/ryanheise/audio_session/AudioSessionPlugin.java
@@ -16,53 +16,53 @@ import java.util.Map;
 
 /** AudioSessionPlugin */
 public class AudioSessionPlugin implements FlutterPlugin, MethodCallHandler {
-	private static Map<?, ?> configuration;
-	private static List<AudioSessionPlugin> instances = new ArrayList<>();
-	private MethodChannel channel;
-	private AndroidAudioManager androidAudioManager;
+    private static Map<?, ?> configuration;
+    private static List<AudioSessionPlugin> instances = new ArrayList<>();
+    private MethodChannel channel;
+    private AndroidAudioManager androidAudioManager;
 
-	@Override
-	public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-		BinaryMessenger messenger = flutterPluginBinding.getBinaryMessenger();
-		channel = new MethodChannel(messenger, "com.ryanheise.audio_session");
-		channel.setMethodCallHandler(this);
-		androidAudioManager = new AndroidAudioManager(flutterPluginBinding.getApplicationContext(), messenger);
-		instances.add(this);
-	}
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        BinaryMessenger messenger = flutterPluginBinding.getBinaryMessenger();
+        channel = new MethodChannel(messenger, "com.ryanheise.audio_session");
+        channel.setMethodCallHandler(this);
+        androidAudioManager = new AndroidAudioManager(flutterPluginBinding.getApplicationContext(), messenger);
+        instances.add(this);
+    }
 
-	@Override
-	public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-		channel.setMethodCallHandler(null);
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
         channel = null;
-		androidAudioManager.dispose();
+        androidAudioManager.dispose();
         androidAudioManager = null;
-		instances.remove(this);
-	}
+        instances.remove(this);
+    }
 
-	@Override
-	public void onMethodCall(final MethodCall call, final Result result) {
-		List<?> args = (List<?>)call.arguments;
-		switch (call.method) {
-		case "setConfiguration": {
-			configuration = (Map<?, ?>)args.get(0);
-			result.success(null);
-			invokeMethod("onConfigurationChanged", configuration);
-			break;
-		}
-		case "getConfiguration": {
-			result.success(configuration);
-			break;
-		}
-		default:
-			result.notImplemented();
-			break;
-		}
-	}
+    @Override
+    public void onMethodCall(final MethodCall call, final Result result) {
+        List<?> args = (List<?>)call.arguments;
+        switch (call.method) {
+        case "setConfiguration": {
+            configuration = (Map<?, ?>)args.get(0);
+            result.success(null);
+            invokeMethod("onConfigurationChanged", configuration);
+            break;
+        }
+        case "getConfiguration": {
+            result.success(configuration);
+            break;
+        }
+        default:
+            result.notImplemented();
+            break;
+        }
+    }
 
-	private void invokeMethod(String method, Object... args) {
-		for (AudioSessionPlugin instance : instances) {
-			ArrayList<Object> list = new ArrayList<Object>(Arrays.asList(args));
-			instance.channel.invokeMethod(method, list);
-		}
-	}
+    private void invokeMethod(String method, Object... args) {
+        for (AudioSessionPlugin instance : instances) {
+            ArrayList<Object> list = new ArrayList<Object>(Arrays.asList(args));
+            instance.channel.invokeMethod(method, list);
+        }
+    }
 }
