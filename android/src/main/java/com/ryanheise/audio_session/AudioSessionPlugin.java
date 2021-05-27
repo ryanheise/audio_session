fff512b0b2d47fe757c4e5a04880eac16894855c
Luke Hartman <dhartman1.dh@gmail.com>
2021-05-26 21:11:10 -0400

    Fixes #24

--- android/src/main/java/com/ryanheise/audio_session/AudioSessionPlugin.java
+++ android/src/main/java/com/ryanheise/audio_session/AudioSessionPlugin.java
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
