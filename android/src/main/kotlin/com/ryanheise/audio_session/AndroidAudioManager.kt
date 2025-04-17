a574a05b42fbfb1cc7a7fc57e7f8fd040a8d6ee8
Abel Putnoki <29568469+putnokiabel@users.noreply.github.com>
2025-04-17 11:34:12 +0200

    Handle null address values with default empty string

--- android/src/main/kotlin/com/ryanheise/audio_session/AndroidAudioManager.kt
+++ android/src/main/kotlin/com/ryanheise/audio_session/AndroidAudioManager.kt
@@ -605,7 +605,7 @@ private class AudioManagerSingleton(applicationContext: Context) {
                 mapOf(
                     "id" to device.id,
                     "productName" to device.getProductName(),
-                    "address" to address,
+                    "address" to address ?: "",
                     "isSource" to device.isSource,
                     "isSink" to device.isSink,
                     "sampleRates" to intArrayToList(device.sampleRates),
@@ -785,7 +785,7 @@ private class AudioManagerSingleton(applicationContext: Context) {
             return mapOf(
                 "id" to device.id,
                 "productName" to device.getProductName(),
-                "address" to address,
+                "address" to address ?: "",
                 "isSource" to device.isSource,
                 "isSink" to device.isSink,
                 "sampleRates" to device.sampleRates,
